ba7f24521a7e59243314bd782f8ecf1660d34ecc
aaaaaaa00n <ahn0000000f@gmail.com>
2026-01-28 09:39:12 +0600

    Delete index.tsx

--- index.tsx
+++
@@ -1,5 +0,0 @@
-// This file is no longer used for the Desktop App version.
-// Please run 'npm install' and 'npm start' to launch the Electron Desktop App.
-// The entry point is now main.js.
-
-console.log("Please run this project using Electron.");
